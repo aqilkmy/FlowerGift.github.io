a4eb2a66f87161cb599ca012910b20f886439645
aqilkmy <aqilkaromy99@gmail.com>
2023-06-03 21:22:07 +0700

    Flower

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 const flower = document.querySelector(".flower");
 const tombol = document.querySelector(".tombol")
 const spin1 = document.querySelector(".spin1")
@@ -23,19 +24,38 @@ const c4 = document.querySelector(".c4")
 const c5 = document.querySelector(".c5")
 const c6 = document.querySelector(".c6")
 
+const d1 = document.querySelector(".d1")
+const d2 = document.querySelector(".d2")
+const d3 = document.querySelector(".d3")
+const d4 = document.querySelector(".d4")
+const d5 = document.querySelector(".d5")
+const d6 = document.querySelector(".d6")
+
+const e1 = document.querySelector(".e1")
+const e2 = document.querySelector(".e2")
+const e3 = document.querySelector(".e3")
+const e4 = document.querySelector(".e4")
+const e5 = document.querySelector(".e5")
+const e6 = document.querySelector(".e6")
 
 const fw1 = document.querySelector(".fw1")
 const fw2 = document.querySelector(".fw2")
 const fw3 = document.querySelector(".fw3")
+const fw4 = document.querySelector(".fw4")
+const fw5 = document.querySelector(".fw5")
 
 const flower1 = document.querySelector(".flower1")
 const flower2 = document.querySelector(".flower2")
 const flower3 = document.querySelector(".flower3")
+const flower4 = document.querySelector(".flower4")
+const flower5 = document.querySelector(".flower5")
 
 spin1.addEventListener("click", () => {
     fw1.classList.toggle("fw1Move")
     fw2.classList.toggle("fw2Move")
     fw3.classList.toggle("fw3Move")
+    fw4.classList.toggle("fw4Move")
+    fw5.classList.toggle("fw5Move")
 
 })
 
@@ -43,6 +63,8 @@ tombol.addEventListener("click", () => {
     fw1.classList.toggle("fw1Move")
     fw2.classList.toggle("fw2Move")
     fw3.classList.toggle("fw3Move")
+    fw4.classList.toggle("fw4Move")
+    fw5.classList.toggle("fw5Move")
 
     a1.classList.toggle("n1")
     a2.classList.toggle("n2")
@@ -82,5 +104,30 @@ tombol.addEventListener("click", () => {
     c4.classList.toggle("flower3on")
     c5.classList.toggle("flower3on")
     c6.classList.toggle("flower3on")
+    
+    d1.classList.toggle("p1")
+    d2.classList.toggle("p2")
+    d3.classList.toggle("p3")
+    d4.classList.toggle("p4")
+    d5.classList.toggle("p5")
+    d6.classList.toggle("p6")
+    d1.classList.toggle("flower4on")
+    d2.classList.toggle("flower4on")
+    d3.classList.toggle("flower4on")
+    d4.classList.toggle("flower4on")
+    d5.classList.toggle("flower4on")
+    d6.classList.toggle("flower4on")
 
+    e1.classList.toggle("q1")
+    e2.classList.toggle("q2")
+    e3.classList.toggle("q3")
+    e4.classList.toggle("q4")
+    e5.classList.toggle("q5")
+    e6.classList.toggle("q6")
+    e1.classList.toggle("flower5on")
+    e2.classList.toggle("flower5on")
+    e3.classList.toggle("flower5on")
+    e4.classList.toggle("flower5on")
+    e5.classList.toggle("flower5on")
+    e6.classList.toggle("flower5on")
 })
